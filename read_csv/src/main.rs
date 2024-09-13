e52b9a9ed8b979f726414428fb87d41938d8b66b
Bode <bode.falade@outlook.com>
2024-09-13 14:54:28 -0500

    setup main and real_main funcs.

--- read_csv/src/main.rs
+++ read_csv/src/main.rs
@@ -1,3 +1,11 @@
+use csv;
+use std::{error::Error, path};
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = read_from_file("./customers.csv") {
+        eprintln!("{}", e);
+    }
+}
+fn read_from_file(path: &str) -> Result<(), Box<dyn Error>>{
+    
 }
