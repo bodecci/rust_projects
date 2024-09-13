2c540754fb839319d14366562575b30acfd8b51d
Bode <bode.falade@outlook.com>
2024-09-13 15:45:25 -0500

    added func to read a csv file.

--- read_csv/src/main.rs
+++ read_csv/src/main.rs
@@ -1,11 +1,19 @@
 use csv;
-use std::{error::Error, path};
+use std::{error::Error, path, result};
 
 fn main() {
     if let Err(e) = read_from_file("./customers.csv") {
         eprintln!("{}", e);
     }
 }
+// In the context of Result<(), Box<dyn Error>>, the () in the Ok(()) 
+//case just means "the function succeeded, but there's no value to return."
 fn read_from_file(path: &str) -> Result<(), Box<dyn Error>>{
-    
+    let mut reader = csv::Reader::from_path(path)?;
+
+    for result in reader.records() {
+        let record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
 }
