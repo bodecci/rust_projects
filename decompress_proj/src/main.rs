69cc88852259dd6a5726d85c4bd810e46b11d9d3
Bode <bode.falade@outlook.com>
2024-09-13 11:51:44 -0500

    setup main and real_main for the actual logic.

---
+++ decompress_proj/src/main.rs
@@ -0,0 +1,12 @@
+use std::fs;
+use std::io;
+
+// to be able to cleanly exit from the real_main()
+fn main() {
+    std::process::exit(real_main())
+}
+
+// the actual logic takes place here
+fn real_main() -> i32 {
+
+}
